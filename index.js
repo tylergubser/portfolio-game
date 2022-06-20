b34bdfd3fad32cb0bd078a231722b50120cdd638
tylergubser <tylergubser2200@gmail.com>
2022-06-20 06:25:07 -0500

    added multiple platform logic

--- index.js
+++ index.js
@@ -53,6 +53,7 @@ class Platform {
 
 const player = new Player()
 const platform = new Platform()
+const platforms = [new Platform()]
 
 const keys = {
     right: {
@@ -68,19 +69,34 @@ function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
     player.update()
-    platform.draw()
+    platforms.forEach(platform => {
+        platform.draw()
+    })
+    
     if (keys.right.pressed && player.position.x < 400) {
         player.velocity.x = 2
     }
     else if (keys.left.pressed && player.position.x > 100) {
         player.velocity.x = -2
-    } else {player.velocity.x = 0
-    if (keys.right.pressed) {platform.position.x -= 5}}
+    } else {
+        player.velocity.x = 0
+    if (keys.right.pressed) { platforms.forEach((platform) => {
+        platform.position.x -= 5
+    })}
+    else if (keys.left.pressed) {
+        platforms.forEach((platform) => {
+            platform.position.x += 5
+        })
+        }
+    } 
+    
 
     // Platform Collision detection
+    platforms.forEach((platform) => {
     if (player.position.y + player.height <= platform.position.y && player.position.y + player.height + player.velocity.y >= platform.position.y && player.position.x + player.width >= platform.position.x && player.position.x <= platform.position.x + platform.width) {
         player.velocity.y = 0
     }
+})
 }
 animate()
 
